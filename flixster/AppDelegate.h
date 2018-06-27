860d452c1a299d047e6e3f54c3d60a2c57b1fe1b
Bevin Benson <bevin@fb.com>
2018-06-27 11:06:04 -0700

    Initial Commit

---
+++ flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flixster
+//
+//  Created by Bevin Benson on 6/27/18.
+//  Copyright © 2018 Bevin Benson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flixster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flixster
+//
+//  Created by Bevin Benson on 6/27/18.
+//  Copyright © 2018 Bevin Benson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
