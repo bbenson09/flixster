ac3ffe8d34225c37fcb225748530a3442402e1c6
Bevin Benson <bevin@fb.com>
2018-06-28 14:45:31 -0700

    Fixed bug in loading animation

--- flixster/View Controllers/MoviesViewController.m
+++ flixster/View Controllers/MoviesViewController.m
@@ -34,6 +34,9 @@
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
     dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
         [self fetchMovies];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+        });
     });
     
     self.refreshControl = [[UIRefreshControl alloc] init];
